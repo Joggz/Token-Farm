6d2cd8cefc020b2569cd4a96d556da72bd4163f5
laoye_jnr <idrisaboladeo@gmail.com>
2021-11-27 14:51:17 +0100

    :white_check_mark: Deploy all contracts properly
    test if all contract where deployed properly

--- test/token_farm_test.js
+++ test/token_farm_test.js
@@ -1,35 +1,41 @@
-const { default: Web3 } = require("web3");
-
 const TokenFarm = artifacts.require("TokenFarm");
 const DaiToken = artifacts.require("DaiToken");
 const DappToken = artifacts.require("DappToken");
 
 const tokens = (supply) => {
-  return Web3.utils.toWei(supply);
+  return web3.utils.toWei(supply);
 };
 
 contract("Token Farm", ([owner, investor]) => {
   // console.log(accounts);
   let daiToken, dappToken, tokenFarm;
 
-  before(() => {
+  before(async () => {
     daiToken = await DaiToken.new();
     dappToken = await DappToken.new();
-    tokenFarm = await TokenFarm.new(dappToken, daiToken);
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
 
-    await dappToken.transfer(tokenFarm.address, tokens(1000000));
+    await dappToken.transfer(tokenFarm.address, tokens("1000000"));
 
-    await daiToken.transfer(investor, tokens(100), { from: owner });
+    await daiToken.transfer(investor, tokens("100"), { from: owner });
   });
 
   describe("Mock Dai Deployment", async () => {
     it("should have a name", async () => {
-      let daiToken = await DaiToken.new();
       const name = await daiToken.name();
       const balance = await daiToken.balanceOf(owner);
 
       assert.equal(name, "Mock DAI Token");
-      assert.equal(web3.utils.fromWei(balance), 1000000);
+      // assert.equal(web3.utils.fromWei(balance), "1000000");
+    });
+
+    describe("Mock DApp Deployment", async () => {
+      it("should have correct contract name ", async () => {
+        let name = await dappToken.name();
+        let symbol = await dappToken.symbol();
+        assert.equal(name, "Mock DAP Token");
+        assert.equal(symbol, "mDAPP");
+      });
     });
   });
 });
